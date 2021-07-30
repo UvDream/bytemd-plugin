8bb49af4ee3a3d51cb1a5aa574c999880d333e56
wangzhongjie <wangzj@cyberpeace.cn>
2021-07-30 18:05:04 +0800

    fix:  修复ignore

--- packages/upload-file/src/index.ts
+++ packages/upload-file/src/index.ts
@@ -2,7 +2,7 @@
  * @Author: wangzhongjie
  * @Date: 2021-07-26 10:22:44
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2021-07-30 17:55:29
+ * @LastEditTime: 2021-07-30 18:00:27
  * @Description:上传文件
  * @Email: UvDream@163.com
  */
